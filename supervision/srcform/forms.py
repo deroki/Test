be3ff9d6622dafe4503f9dc8354caa1cbc90d5b1
DESKTOP-IJFNECO\linxs <linxsaturnos@gmail.com>
2018-05-10 17:41:10 +0200

    selenium takes form info and class PCR in process

--- supervision/srcform/forms.py
+++ supervision/srcform/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
 class SiteForm(forms.Form):
-    site_id = forms.CharField(label='nombre del site',required= True,  max_length= 100)
+    site_id = forms.CharField(label='id del site',required= True,  max_length= 100)
     site_name = forms.CharField(label='nombre del site', max_length=100)
+    site_ip = forms.IntegerField(label = 'ip del site')
